d2d9ffecf644d1ca58f1c4b2d7fd878536e0b9d9
Dylan Shine <dylan@shinelabs.dev>
2021-03-13 16:02:13 -0700

    Update configure function to be static

--- Sources/XSJWT/XSJWT.swift
+++ Sources/XSJWT/XSJWT.swift
@@ -2,7 +2,7 @@ import Vapor
 import JWT
 
 public enum Configurer {
-    public func configure(_ app: Application) throws {
+    public static func configure(_ app: Application) throws {
         
         guard let fileName = Environment.get("JWKS_KEYPAIR_FILE") else {
             fatalError("Please add JWKS filename to environment variables")
